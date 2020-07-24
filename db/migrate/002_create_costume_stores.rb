fe1babfadb8bd1f6953bbc330a3df83979c49a90
Shreemangal Sethi <shreemangal.sethi@gmail.com>
2020-07-24 17:24:14 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -7,11 +7,11 @@ class CreateCostumeStores < ActiveRecord::Migration[5.2]
       t.string :name
       t.string :location
       t.integer :costume_inventory
-      t.integer :employees_count
+      t.integer :num_of_employees
       t.boolean :still_in_business
       t.string :size
       t.string :image_url
-      t.datetime :opening_time
+      t.datetime :start_time
       t.datetime :closing_time
     end
   end  
