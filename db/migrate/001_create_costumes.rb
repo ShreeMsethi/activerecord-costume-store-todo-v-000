d028164862c2185096e986b234eded63fe50e619
Shreemangal Sethi <shreemangal.sethi@gmail.com>
2020-07-24 17:10:50 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -16,7 +16,8 @@ class CreateCostumes < ActiveRecord::Migration[5.2]
       t.integer :price
       t.string :size
       t.string :image_url
-      t.timestamps
+      t.timestamps :created_at
+      t.timestamps :updated_at
     end  
   end
 
