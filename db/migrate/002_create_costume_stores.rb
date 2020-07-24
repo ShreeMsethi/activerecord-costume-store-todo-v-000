c636fadbb452f8bfe7ab15e987e1b746bca67ff2
Shreemangal Sethi <shreemangal.sethi@gmail.com>
2020-07-24 16:58:04 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -4,15 +4,15 @@ class CreateCostumeStores < ActiveRecords::Migration[5.2]
 
   def change
     create_table :costume_stores |t|
-    t.string :name
-    t.string :location
-    t.integer :costume_inventory
-    t.integer :number_of_emplyees
-    t.boolean :still_in_business
-    t.string :size
-    t.string :image_url
-    t.datetime :openning_time
-    t.datetime :closing_time
+      t.string :name
+      t.string :location
+      t.integer :costume_inventory
+      t.integer :number_of_emplyees
+      t.boolean :still_in_business
+      t.string :size
+      t.string :image_url
+      t.datetime :openning_time
+      t.datetime :closing_time
     end
   end  
 
