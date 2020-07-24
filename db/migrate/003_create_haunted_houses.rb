fef1678602a6416d36fdded91822117f31605831
Shreemangal Sethi <shreemangal.sethi@gmail.com>
2020-07-24 17:06:40 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -2,7 +2,7 @@
 class CreateHauntedHouses < ActiveRecords::Migration[5.2]
   
   def change 
-    create_table :haunted_houses |t|
+    create_table :haunted_houses do |t|
       t.string :name
       t.string :location
       t.string :theme 
