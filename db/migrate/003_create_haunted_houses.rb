2582245289facbb8ee3b2f209387e7242f30e586
Shreemangal Sethi <shreemangal.sethi@gmail.com>
2020-07-24 17:49:19 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -8,9 +8,9 @@ class CreateHauntedHouses < ActiveRecord::Migration[5.2]
       t.string :theme 
       t.integer :price
       t.boolean :family_friendly
-      t.datetime :opening_date
+      t.datetime :openning_date
       t.datetime :closing_date
-      t.text :description
+      t.string :description
     end
   end
 end  
